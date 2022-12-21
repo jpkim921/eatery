8c318a965401e7c5114c06c14406cced68ac7d2a
Jayon Kim <jpkim921@gmail.com>
2022-12-21 10:35:30 -0500

    first commit. create initial django project with two inital apps - customer and restaurant. create index, menu, and about views

---
+++ Readme.md
@@ -0,0 +1,11 @@
+
+
+
+
+
+Notes:
+ - admin/q
+
+
+Photo Credits:
+ - Photo by Rachel Claire: https://www.pexels.com/photo/interior-design-of-asian-bar-5531434/
---
+++ eatery/customer/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+class Index(View):
+    def get(self, request, * args, **kwargs):
+        return render(request, 'customer/index.html')
+
+class Menu(View):
+    def get(self, request, * args, **kwargs):
+        return render(request, 'customer/menu.html')
+
+class About(View):
+    def get(self, request, * args, **kwargs):
+        return render(request, 'customer/about.html')
