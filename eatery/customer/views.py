125be6da3d81242926af06faa81bcae7ff8284f0
Jayon Kim <jpkim921@gmail.com>
2022-12-23 15:55:48 -0500

    add order view and create some data for db

--- eatery/customer/views.py
+++ eatery/customer/views.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 from django.shortcuts import render
 from django.views import View
+from .models import MenuItem, Category, OrderModel
 
 class MealType(Enum):
     BREAKFAST = 'breakfast'
@@ -10,12 +11,12 @@ class MealType(Enum):
 
 # Create your views here.
 class Index(View):
-    def get(self, request, * args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return render(request, 'customer/index.html')
 
 class HomeMenu(View):
 
-    def get(self, request, * args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return render(request, 'customer/home_menu.html')
 
     def post(self, request, *args, **kwargs):
@@ -40,7 +41,7 @@ class HomeMenu(View):
 
 
 class BreakfastMenu(View):
-    def get(self, request, * args, **kwargs):
+    def get(self, request, *args, **kwargs):
         context = {
             'menu_items': [
                 {
@@ -56,12 +57,62 @@ class BreakfastMenu(View):
         }
         return render(request, 'customer/breakfast_menu.html', context=context)
 class LunchMenu(View):
-    def get(self, request, * args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return render(request, 'customer/lunch_menu.html')
 class DinnerMenu(View):
-    def get(self, request, * args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return render(request, 'customer/dinner_menu.html')
 
 class About(View):
-    def get(self, request, * args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
+
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        appetizers = MenuItem.objects.filter(category__name__contains='Appetizer')
+        entrees = MenuItem.objects.filter(category__name__contains='Entree')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
+
+        context = {
+            'appetizers': appetizers,
+            'entrees': entrees,
+            'desserts': desserts,
+            'drinks': drinks,
+        }
+        print('context', context)
+        return render(request, 'customer/order.html', context=context)
+
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items': []
+        }
+        price = 0
+        item_ids = []
+
+        items = request.POST.getlist('items[]')
+
+        # iterate through items and get the menu item and price from db
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item)) 
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
+            }
+            order_items['items'].append(item_data)
+            price += menu_item.price
+            item_ids.append(menu_item.pk)
+        
+
+        # create the order
+        order = OrderModel.objects.create(price=price)
+        order.items.add(*item_ids)
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+        return render(request, 'customer/order_confirmation.html', context=context)
+        
+
