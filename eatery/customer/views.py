e694ad65fb0446d972f193478ae00dc3b020465f
Jayon Kim <jpkim921@gmail.com>
2022-12-22 19:54:23 -0500

    change from link to hidden form for menu meal type links

--- eatery/customer/views.py
+++ eatery/customer/views.py
@@ -1,18 +1,60 @@
+from enum import Enum
+
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
+class MealType(Enum):
+    BREAKFAST = 'breakfast'
+    LUNCH = 'lunch'
+    DINNER = 'dinner'
 
+# Create your views here.
 class Index(View):
     def get(self, request, * args, **kwargs):
         return render(request, 'customer/index.html')
 
 class HomeMenu(View):
+
     def get(self, request, * args, **kwargs):
         return render(request, 'customer/home_menu.html')
+
+    def post(self, request, *args, **kwargs):
+        # meal = MealType()
+        context = {}
+        # print('meal_type', request.POST['meal_type'])
+        meal_type = request.POST['meal_type']
+        context['meal'] = meal_type
+        # search db based on request.POST['meal_type']
+
+        print(context)
+        return render(request, 'customer/home_menu.html', context=context)
+
+
+
+        # form = self.form_class(request.POST)
+        # if form.is_valid():
+        #     # <process form cleaned data>
+        #     return HttpResponseRedirect('/success/')
+
+        # return render(request, self.template_name, {'form': form})
+
+
 class BreakfastMenu(View):
     def get(self, request, * args, **kwargs):
-        return render(request, 'customer/breakfast_menu.html')
+        context = {
+            'menu_items': [
+                {
+                    'name': 'Lo Mein'
+                },
+                {
+                    'name': 'Gen Tso'
+                },
+                {
+                    'name': 'Dumplings'
+                },
+            ]
+        }
+        return render(request, 'customer/breakfast_menu.html', context=context)
 class LunchMenu(View):
     def get(self, request, * args, **kwargs):
         return render(request, 'customer/lunch_menu.html')
