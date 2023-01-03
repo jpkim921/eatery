701a177dd22ab610a03eba68405c73c9dcb997bb
Jayon Kim <jpkim921@gmail.com>
2023-01-02 23:02:24 -0500

    add additional fields to order model

--- eatery/customer/models.py
+++ eatery/customer/models.py
@@ -32,6 +32,12 @@ class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
+    name = models.CharField(max_length=200, blank=True)
+    email = models.CharField(max_length=200, blank=True)
+    street = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=10, blank=True)
+    zipcode = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
