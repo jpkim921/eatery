d69ccd65bcbc8dc86cabfa42f67e838d9aeed6cc
Jayon Kim <jpkim921@gmail.com>
2023-01-03 03:13:57 -0500

    fix redirect to payment confirmation

--- eatery/eatery/urls.py
+++ eatery/eatery/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('about/', About.as_view(), name='about'),
     path('order/', Order.as_view(), name='order'),
     path('order-confirmation/<int:pk>/', OrderConfirmation.as_view(), name='order-confirmation'),
-    path('payment-confirmation', OrderPayConfirmation.as_view(), name='payment_confirmation'),
+    path('payment-confirmation/', OrderPayConfirmation.as_view(), name='payment_confirmation'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
